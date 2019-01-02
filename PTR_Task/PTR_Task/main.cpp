f86242fc84ae6e8b26cd4a0ba8865da98e1ca75d
ruslanist <istominruslan@mail.ru>
2019-01-03 01:25:03 +0800

    Change_exchangeAnalyst

--- PTR_Task/PTR_Task/main.cpp
+++ PTR_Task/PTR_Task/main.cpp
@@ -7,54 +7,51 @@ using std::min_element;
 using std::max_element;
 
 double mass[] = {10, 24, 74, 43, 56, 67, 39, 15, 99, 25, 17, 13, 19, 44, 25, 90, 27, 47, 34, 15, 86, 57, 68, 90, 10, 11, 22, 13, 44, 15};
-double size_began[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-double size_end[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
-double *ptr_mass = mass; // ****** Указатель на массив
-size_t len_mass = sizeof(mass) / sizeof(mass[0]); // ****** Длина Массива
-
-size_t range_Len = sizeof(size_began) / sizeof(size_began[0]); // ****** Длина 1-й Части Масива
-size_t breakout_Len = sizeof(size_end) / sizeof(size_end[0]); // ******* Длина второй части Массива
+size_t len_mass = sizeof(mass) / sizeof(mass[0]); // Длина Массива
 
 void exchangeAnalyst(double *ptr, size_t len, size_t range_len, size_t breakout_len) {
 
-    double *ptrMas1 = ptr + range_Len;
-    double *ptrMas2 = ptr + len_mass;
+    double *ptrMas1 = ptr + range_len;
+    double *ptrMas2 = ptr + len;
 
     double *cursor1 = ptr;
     double *cursor2 = ptr + range_len ;
 
+
     cout << "Вывод первой части Массива" << endl;
 
+    double *incr1 = cursor1;
+    while( incr1 != ptrMas1) {
 
-    while(cursor1 != ptrMas1) {
+        cout << *incr1 << endl;
 
-        cout << *cursor1 << endl;
-
-        cursor1++;
+        incr1++;
     }
 
     cout << "Вывод второй части Массива" << endl;
 
-    while(cursor2 != ptrMas2) {
+    double *incr2 = cursor2;
+    while(incr2 != ptrMas2) {
 
-        cout << *cursor2 << endl;
+        cout << *incr2 << endl;
 
-        cursor2++;
+        incr2++;
     }
 
-    cout << "Нахожждение Минемального значения с 1 - 10 включительно  число месяца" << endl;
-    min_element(cursor1, ptrMas1);
+    cout << "Нахожждение Минемального значения первой части массива" << endl;
+    cout << min_element(cursor1, ptrMas1) << endl;
 
-    cout << "Нахожждение Минемального значение с 11-го - 30 включительно  число месяца" << endl;
-    min_element(cursor2, ptrMas2);
+    cout << "Нахожждение Минемального значение второй части массива" << endl;
+    cout << min_element(cursor2, ptrMas2) << endl;
 }
-
-
 
 int main()
 {
-    exchangeAnalyst(ptr_mass, len_mass, range_Len, breakout_Len);
+    size_t range_Len = 10; // Test-Длина 1-й Части Масива
+    size_t breakout_Len = 20; // Test-Длина второй части Массива
+
+    exchangeAnalyst(mass, len_mass, range_Len, breakout_Len);
 
 
     cout << "Hello World!" << endl;
