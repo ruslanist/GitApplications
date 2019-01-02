8f7a027e5a0a90372d37a3ce31b186dd2b571b19
ruslanist <istominruslan@mail.ru>
2019-01-03 01:56:08 +0800

    Change_exchangeAnalyst2

--- PTR_Task/PTR_Task/main.cpp
+++ PTR_Task/PTR_Task/main.cpp
@@ -6,44 +6,39 @@ using std::endl;
 using std::min_element;
 using std::max_element;
 
-double mass[] = {10, 24, 74, 43, 56, 67, 39, 15, 99, 25, 17, 13, 19, 44, 25, 90, 27, 47, 34, 15, 86, 57, 68, 90, 10, 11, 22, 13, 44, 15};
+double mass[] = {10, 24, 74, 43, 56, 67, 39, 15, 99, 25, 17, 13, 19, 44, 25, 90, 27, 47, 34, 15, 86, 57, 68, 90, 10, 11, 9, 13, 44, 15};
 
-size_t len_mass = sizeof(mass) / sizeof(mass[0]); // Длина Массива
-
-void exchangeAnalyst(double *ptr, size_t len, size_t range_len, size_t breakout_len) {
+void exchangeAnalyst(double *ptr, size_t range_len, size_t breakout_len) {
 
     double *ptrMas1 = ptr + range_len;
-    double *ptrMas2 = ptr + len;
+    double *ptrMas2 = ptr + range_len + breakout_len;
 
     double *cursor1 = ptr;
     double *cursor2 = ptr + range_len ;
 
-
     cout << "Вывод первой части Массива" << endl;
 
-    double *incr1 = cursor1;
-    while( incr1 != ptrMas1) {
+    while( cursor1 != ptrMas1) {
 
-        cout << *incr1 << endl;
+        cout << *cursor1 << endl;
 
-        incr1++;
+        cursor1++;
     }
 
     cout << "Вывод второй части Массива" << endl;
 
-    double *incr2 = cursor2;
-    while(incr2 != ptrMas2) {
+    while(cursor2 != ptrMas2) {
 
-        cout << *incr2 << endl;
+        cout << *cursor2 << endl;
 
-        incr2++;
+        cursor2++;
     }
 
     cout << "Нахожждение Минемального значения первой части массива" << endl;
-    cout << min_element(cursor1, ptrMas1) << endl;
+    cout << *min_element(ptr, ptr + range_len) << endl;
 
     cout << "Нахожждение Минемального значение второй части массива" << endl;
-    cout << min_element(cursor2, ptrMas2) << endl;
+    cout << *min_element(ptr + range_len, ptr + range_len + breakout_len) << endl;
 }
 
 int main()
@@ -51,7 +46,7 @@ int main()
     size_t range_Len = 10; // Test-Длина 1-й Части Масива
     size_t breakout_Len = 20; // Test-Длина второй части Массива
 
-    exchangeAnalyst(mass, len_mass, range_Len, breakout_Len);
+    exchangeAnalyst(mass, range_Len, breakout_Len);
 
 
     cout << "Hello World!" << endl;
