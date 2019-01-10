ddc8f69587641ff30933ebce455f16614564cf75
ruslanist <istominruslan@mail.ru>
2019-01-10 19:20:30 +0800

    CreatemongoBaseApllication

---
+++ mongoBase/mongoBase/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include "mongo/client/dbclient.h"
+#include <vector>
+
+using namespace std;
+using namespace bson;
+
+int main() {
+  try {
+
+    std::cout << "connected ok" << std::endl;
+
+    mongo::DBClientConnection c;
+    c.connect("localhost");
+
+    vector<double> adjusted_close;
+
+    cout << "count:" << c.count("us_stocks.ZYME.US") << endl;
+    auto_ptr<mongo::DBClientCursor> cursor = c.query("us_stocks.ZYME.US", mongo::BSONObj());
+    while (cursor->more()) {
+       cout << cursor->next().toString() << endl;
+       mongo::BSONElement name =  cursor->next().getField("adjusted_close");
+       cout << name << endl;
+    }
+
+  } catch( const mongo::DBException &e ) {
+    std::cout << "caught " << e.what() << std::endl;
+  }
+  return EXIT_SUCCESS;
+}
+
+
+
+
