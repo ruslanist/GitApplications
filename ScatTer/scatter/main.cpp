2f0f40a874aea0f51853754792c5952dc3b9d579
ruslan <istominruslan@mail.ru>
2019-04-08 02:51:15 +0800

    Relase2

--- ScatTer/scatter/main.cpp
+++ ScatTer/scatter/main.cpp
@@ -1,3 +1,5 @@
+//  Реализация №1
+/*
 #include <iostream>
 #include <iterator>
 #include <vector>
@@ -26,6 +28,44 @@ int main()
 
 
     cout << "Hello World!" << endl;
+
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+template <typename InputIt, typename OutputIt>
+OutputIt scatter(InputIt first, InputIt last, OutputIt result) {
+
+    while (first != last) {
+
+        *result++ = *first++;
+
+        return result;
+    }
+
+}
+
+int main()
+{
+
+    vector<int> myVec{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    vector<int> myVec1;
+
+    vector<int>::iterator it_ferst;
+    vector<int>::iterator it_last;
+    vector<int>::iterator it_result;
+
+    it_ferst = myVec.begin();
+    it_last = myVec.end();
+    it_result = myVec1.begin();
+
+    scatter(it_ferst, it_last, it_result);
 
     return 0;
 }
